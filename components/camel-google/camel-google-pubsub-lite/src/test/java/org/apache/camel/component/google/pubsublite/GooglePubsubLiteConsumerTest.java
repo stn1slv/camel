b20f8768b7f85fe28799865ed753ed765f636220
Stanislav Deviatov <devyatov@gmail.com>
2024-03-26 18:08:22 +0100

    Update GooglePubsubLiteConsumerTest.java

--- components/camel-google/camel-google-pubsub-lite/src/test/java/org/apache/camel/component/google/pubsublite/GooglePubsubLiteConsumerTest.java
+++ components/camel-google/camel-google-pubsub-lite/src/test/java/org/apache/camel/component/google/pubsublite/GooglePubsubLiteConsumerTest.java
@@ -16,8 +16,10 @@
  */
 package org.apache.camel.component.google.pubsublite;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.DefaultExchange;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,7 +27,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class GooglePubsubLiteConsumerTest extends CamelTestSupport {
 
@@ -45,6 +49,23 @@ public class GooglePubsubLiteConsumerTest extends CamelTestSupport {
         when(endpoint.getCamelContext()).thenReturn(context);
         GooglePubsubLiteConsumer consumer = new GooglePubsubLiteConsumer(endpoint, processor);
         assertNotNull(consumer);
+    }
+
+    @Test
+    public void testGooglePubsubLiteConsumerException() throws Exception {
+        when(endpoint.getCamelContext()).thenReturn(context);
+        GooglePubsubLiteConsumer consumer = new GooglePubsubLiteConsumer(endpoint, processor);
+        assertNotNull(consumer);
+
+        // Assuming the processor is throwing an exception
+        doThrow(new RuntimeException("Mocked Exception")).when(processor).process(any());
+
+        // Creating a new exception to test the process
+        Exchange exchange = new DefaultExchange(context);
+
+        assertThrows(RuntimeException.class, () -> processor.process(exchange));
+        // Verifying processor was called
+        verify(processor, times(1)).process(any());
     }
 
     @Override
